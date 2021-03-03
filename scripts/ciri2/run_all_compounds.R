306b52b7f17c2ad7e88cf30e88a45f9c882572ad
Juan Ochoteco <jochotecoa@gmail.com>
2021-03-03 18:33:33 +0100

    ciri2 scripts

---
+++ scripts/ciri2/run_all_compounds.R
@@ -0,0 +1,42 @@
+tissue="Hepatic"
+
+compound="AZA" #
+compid="Azathioprine"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="CYC" #
+compid="Cyclosporin"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="DIC" #
+compid="Diclofenac"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="ISO" #
+compid="Isoniazid"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="MTX" #
+compid="Methotrexate"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="PHE" #
+compid="Phenytoin"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+compound="RIF" #
+compid="Rifampicin"
+. scripts/ciri2/ciri2v6.1_multiple.sh
+
+# compound="VPA" #
+# compid="Valproic_Acid"
+# . scripts/ciri2/ciri2v6.1_multiple.sh
+# 
+# compound="ConDMSO" #
+# compid="Con_0.1_DMSO"
+# . scripts/ciri2/ciri2v6.1_multiple.sh
+# 
+# compound="APA" #
+# compid="Acetaminophen"
+# . scripts/ciri2/ciri2v6.1_multiple.sh
+
